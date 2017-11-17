749a1d832c36d4dca75cd7788af26101e887690d
kamijenkins <32181383+kamijenkins@users.noreply.github.com>
2017-11-16 23:43:03 -0500

    finalproduct
    updated readme, added comments

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-
-
-//$('#myModal').on('shown.bs.modal', function () {
-//  $('#myInput').trigger('focus')
-//})
-
+// Alert diplayed first time visiting page //
 if(!localStorage["alertdisplayed"]) {
+//Check localStorage to what is currently held there
     alert("Don't forget to sign up for our E-Newsletter at the bottom of the page to keep up on all the latest happenings at LYG!")
+//Alert text
     localStorage["alertdisplayed"] = true;
+//Check to see if the alert has been displayed before
 }
 
+// Alert confirming email subscription in Modal //
 function myFunction() {
+//Anonymous function
     alert("Thanks for subscribing!");
+//Display when user subscribes
 }
